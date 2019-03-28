0d6166f3f11c135d50c2dbac9fc629d6f28f2c6a
carinareketis <carina.reketis@gmail.com>
2019-03-28 12:30:57 -0300

    botão login e face funcionando

--- src/scripts/funcoes.js
+++ src/scripts/funcoes.js
@@ -1,26 +1,26 @@
-//DELETAR UM REGISTRO NO BD
-<html>
-<head>
-<script language="Javascript">
-function confirmacao(id) {
-     let resposta = confirm("Deseja remover esse registro?");
+// //DELETAR UM REGISTRO NO BD
+// <html>
+// <head>
+// <script language="Javascript">
+// function confirmacao(id) {
+//      let resposta = confirm("Deseja remover esse registro?");
  
-     if (resposta == true) {
-          window.location.href = "remover.php?id="+id;
-     }
-}
-</script>
-</head>
+//      if (resposta == true) {
+//           window.location.href = "remover.php?id="+id;
+//      }
+// }
+// </script>
+// </head>
  
-<body>
-<a href="javascript:func()"
-onclick="confirmacao('1')">Remover registro #1</a>
+// <body>
+// <a href="javascript:func()"
+// onclick="confirmacao('1')">Remover registro #1</a>
  
-<a href="javascript:func()"
-onclick="confirmacao('2')">Remover registro #2</a>
+// <a href="javascript:func()"
+// onclick="confirmacao('2')">Remover registro #2</a>
  
-<a href="javascript:func()"
-onclick="confirmacao('3')">Remover registro #3</a>
-</body>
-</html>
+// <a href="javascript:func()"
+// onclick="confirmacao('3')">Remover registro #3</a>
+// </body>
+// </html>
 
