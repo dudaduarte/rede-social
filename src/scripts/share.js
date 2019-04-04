7c2d63ddba0b270524b4b3511ed5e418227726c6
carinareketis <carina.reketis@gmail.com>
2019-04-03 22:44:09 -0300

    função de desabilitar botão ok

---
+++ src/scripts/share.js
@@ -0,0 +1,15 @@
+$('#user-message').keyup(function() {
+
+    if( $(this).val().length > 0 ) {
+            
+        $('#btn-share').attr('disabled', false);
+               
+    }
+    
+    else {
+            
+        $('#btn-share').attr('disabled', true);
+            
+    }
+            
+});
