538e542016939ed48e077e128d9deae661e25ee3
Duda Duarte <dudammduarte@yahoo.com.br>
2019-04-09 22:26:57 -0300

    ajustes no perfil

--- src/scripts/profile.js
+++ src/scripts/profile.js
@@ -11,5 +11,10 @@ $(document).ready(function () {
         $('#display-pnumber').html(!user.pnumber? "Não disponível" : user.pnumber);
         $('#display-school').html(!user.school? "Não informado" : user.school);
         $('#user-bio').html(!user.bio? "Fale um pouco de você aqui." : user.bio);
+
+        $('#posts').append(`
+        
+        `)
     })
 })
+
