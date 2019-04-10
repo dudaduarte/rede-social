49a4c7a788b3ef339deb3663c724a512c59d2247
Duda Duarte <dudammduarte@yahoo.com.br>
2019-04-09 21:53:51 -0300

    mudanças mínimas

--- src/scripts/profile.js
+++ src/scripts/profile.js
@@ -4,14 +4,12 @@ $(document).ready(function () {
 
     database.ref(`users/${USER_ID}`).once('value', function (snapshot) {
         let user = snapshot.val();
-
-        console.log(user);
+        
         $('#display-name, #display-name-small').html(user.name);
         $('#display-status').html(!user.status? "Set Status" : user.status);
         $('#display-email').html(user.email);
         $('#display-pnumber').html(!user.pnumber? "Não disponível" : user.pnumber);
         $('#display-school').html(!user.school? "Não informado" : user.school);
+        $('#user-bio').html(!user.bio? "Fale um pouco de você aqui." : user.bio);
     })
-
-
 })
